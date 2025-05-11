06e6feaf0faeda4b767b847d11883b3be3d09b7b
Vivek Mehta <126068703+VivekMehta16@users.noreply.github.com>
2025-05-10 18:35:49 -0700

    Time: 28 ms (79.19%), Space: 20.3 MB (48.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,17 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        pre = [1] * n
-        post = [1] * n
-        res = [0] * n
+        result = [1] * n
 
-        for i in range(1,n):
-            pre[i] = pre[i-1] * nums[i-1]
-
-        for i in range(n-2, -1, -1):
-            post[i] = post[i+1] * nums[i+1]
-
+        # Build prefix in result array
+        prefix = 1
         for i in range(n):
-            res[i] = pre[i] * post[i]
+            result[i] = prefix
+            prefix *= nums[i]
 
-        return res
+        # Multiply with postfix on the fly
+        postfix = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+
+        return result
